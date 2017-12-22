161d55aff39fe34467c28ac3c843bcbbca248e8d
Filip Szczepanik <filip.szczepanik@gmail.com>
2017-12-22 11:07:11 +0100

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => (
+    <div>
+      <div className="welcome">
+        Test scss
+      </div>
+      <h2>Home</h2>
+    </div>
+  )
+
+
+export default Home
