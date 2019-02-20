a0ca99d68d7c28d46f282e0181bf8641624070fc
unknown <szef0>
2019-02-20 20:59:24 +0100

    fix hash

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <React.Fragment>
           <Menu />
           <Route path="/" component={Home} />
-          <Route path="/about" component={About} />
+          <Route path="react/about" component={About} />
         </React.Fragment>
       </HashRouter>
     );
